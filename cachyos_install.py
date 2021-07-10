1e3babe294f9478ebfb66e16c39e389d2243e736
Hamad Al Marri <hamad@cachyos.org>
2021-07-10 21:05:27 +0300

    keyring to v8

--- cachyos_install.py
+++ cachyos_install.py
@@ -170,7 +170,7 @@ def print_separator(current_step = ""):
 
 def add_cachyos_keyring(installation):
 	print(f"\n{bcolors.GRAY}Adding CachyOS keyring...\n{bcolors.ENDC}")
-	_keypkgname = "keyring-cachyos-1-7-x86_64.pkg.tar.zst"
+	_keypkgname = "keyring-cachyos-1-8-x86_64.pkg.tar.zst"
 	commands = [f"pacman --noconfirm -U /root/{_keypkgname}"]
 
 	os.system(f"cp /root/{_keypkgname} {installation.target}/root/")
