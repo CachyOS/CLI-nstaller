1800220ef0e0b0e230260f96143daff5b782d32a
Hamad Al Marri <hamad@cachyos.org>
2021-07-17 18:58:45 +0300

    chenge to linux-cachyos to linux-cacule
    https://gitea.cachyos.org/CachyOS/cachyos-installer/issues/18

--- cachyos_install.py
+++ cachyos_install.py
@@ -25,7 +25,7 @@ cachy_offline = False
 essentials = 	["linux-headers", "vi", "nano", "fish", "bash-completion", "git"]
 
 cachyos_gpg_key_url = "https://raw.githubusercontent.com/CachyOS/PKGBUILDS/master/keyring-cachyos/cachyos.gpg"
-cachyos_packages = "linux-cachyos linux-cachyos-headers cachyos-settings paru-bin "
+cachyos_packages = "linux-cacule linux-cacule-headers cachyos-settings paru-bin "
 cachyos_kde_theme = "cachyos-emerald-kde-theme-git "
 
 minimum_kde_packages =	["xorg", "plasma-desktop", "plasma-framework", "plasma-nm", "plasma-pa",
