5402e9976cae719a6ded40b100864f05b439b3c8
ptr1337 <admin@ptr1337.dev>
2021-08-05 20:04:22 +0000

    „cachyos_install.py“ ändern

--- cachyos_install.py
+++ cachyos_install.py
@@ -43,7 +43,7 @@ rec_kde_packages =	["bluedevil", "drkonqi", "kde-gtk-config", "kdeplasma-addons"
 full_kde_packages =	["plasma-meta", "kde-applications-meta"]
 
 # Browsers
-browser = "librewolf "
+browser = "cachy-browser "
 
 # Graphics Drivers
 Xorg_Intel_pa = ["xf86-video-intel"]
